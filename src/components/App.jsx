9afc93ef76e3c4cd81122fba9618ee7737a67a32
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-07-24 00:37:33 +0300

    finally logik done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,15 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   useEffect(() => {
     const getData = async () => {
       try {
         setIsLoading(true);
         setIsError(false);
-        const response = await fetchPhotos(query, page, 3);
+        const response = await fetchPhotos(query, page, 10);
         setPhotos(prev => [...prev, ...response.results]);
+        setTotalPages(response.total_pages);
       } catch (error) {
         console.log(error);
         setIsError(true);
@@ -41,14 +43,16 @@ const App = () => {
       <ImageGallery photos={photos} />
       {isLoading && <Loader />}
       {isError && <h2>Something went wrong! Try again...</h2>}
-      <button
-        type="button"
-        onClick={() => {
-          setPage(prev => prev + 1);
-        }}
-      >
-        Load more
-      </button>
+      {totalPages > page && !isLoading && (
+        <button
+          type="button"
+          onClick={() => {
+            setPage(prev => prev + 1);
+          }}
+        >
+          Load more
+        </button>
+      )}
     </div>
   );
 };
